25adf224cfd03516eadc475373d88206757da516
Kyle <aofeng73@outlook.com>
2025-12-07 01:05:30 +0800

    NEW

--- luci-app-fan-control/luasrc/controller/fan_control.lua
+++ luci-app-fan-control/luasrc/controller/fan_control.lua
@@ -5,65 +5,112 @@ function index()
         return
     end
 
-    entry({"admin", "services", "fan_control"}, cbi("fan_control"), _("Fan Control"), 90).dependent = true
-    entry({"admin", "services", "fan_control", "status"}, call("get_status")).leaf = true
+    entry({"admin", "services", "fan_control"}, call("index_page"), _("Fan Control"), 90).dependent = true
+    entry({"admin", "services", "fan_control", "get_devices"}, call("get_devices")).leaf = true
+    entry({"admin", "services", "fan_control", "get_config"}, call("get_config")).leaf = true
+    entry({"admin", "services", "fan_control", "save_config"}, call("save_config")).leaf = true
+    entry({"admin", "services", "fan_control", "list_map"}, call("list_map")).leaf = true
     entry({"admin", "services", "fan_control", "add_map"}, call("add_map")).leaf = true
     entry({"admin", "services", "fan_control", "delete_map"}, call("delete_map")).leaf = true
-    entry({"admin", "services", "fan_control", "list_map"}, call("list_map")).leaf = true
 end
 
-function get_status()
-    local uci = require "luci.model.uci".cursor()
+function index_page()
+    luci.template.render("fan_control/index")
+end
+
+function get_devices()
     local fs = require "nixio.fs"
     local json = require "luci.jsonc"
-    
-    local response = {}
-    
-    local sensor = uci:get("fan_control", "fan_control", "sensor")
-    if sensor and fs.access(sensor) then
-        for file in fs.dir(sensor) do
-            if file:match("temp%d+_input") then
-                local temp_file = sensor .. "/" .. file
-                if fs.access(temp_file) then
-                    local raw_temp = fs.readfile(temp_file)
-                    if raw_temp then
-                        local temp_c = tonumber(raw_temp) / 1000
-                        response.current_temp = string.format("%.1f", temp_c)
+
+    local sensors = {}
+    local fans = {}
+
+    local hwmon_dir = "/sys/class/hwmon"
+    if fs.access(hwmon_dir) then
+        for file in fs.dir(hwmon_dir) do
+            if file:match("^hwmon%d+$") then
+                local path = hwmon_dir .. "/" .. file
+                local name_file = path .. "/name"
+                local device_name = "Unknown"
+
+                if fs.access(name_file) then
+                    local name_content = fs.readfile(name_file)
+                    if name_content then
+                        device_name = name_content:gsub("%s+", "")
                     end
                 end
-                break
+
+                -- Check for temperature sensors
+                local has_temp = false
+                for subfile in fs.dir(path) do
+                    if subfile:match("^temp%d+_input$") then
+                        has_temp = true
+                        break
+                    end
+                end
+
+                if has_temp then
+                    sensors[path] = device_name .. " (" .. file .. ")"
+                end
+
+                -- Check for PWM fans
+                local has_pwm = false
+                for subfile in fs.dir(path) do
+                    if subfile:match("^pwm%d+$") and not subfile:match("_enable$") then
+                        has_pwm = true
+                        break
+                    end
+                end
+
+                if has_pwm then
+                    fans[path] = device_name .. " (" .. file .. ")"
+                end
             end
         end
     end
-    
-    local fan = uci:get("fan_control", "fan_control", "fan")
-    if fan and fs.access(fan) then
-        for file in fs.dir(fan) do
-            if file:match("pwm%d+$") then
-                local pwm_file = fan .. "/" .. file
-                if fs.access(pwm_file) then
-                    local raw_pwm = fs.readfile(pwm_file)
-                    if raw_pwm then
-                        local pwm_val = tonumber(raw_pwm)
-                        local speed_pct = math.floor((pwm_val / 255) * 100)
-                        response.current_fan_speed = speed_pct
-                    end
-                end
-                break
-            end
-        end
-    end
-    
-    response.enabled = uci:get("fan_control", "fan_control", "enabled") == "1"
-    
+
     luci.http.prepare_content("application/json")
-    luci.http.write(json.stringify(response))
+    luci.http.write(json.stringify({sensors = sensors, fans = fans}))
+end
+
+function get_config()
+    local uci = require "luci.model.uci".cursor()
+    local json = require "luci.jsonc"
+
+    local config = {
+        enabled = uci:get("fan_control", "fan_control", "enabled") or "0",
+        sensor = uci:get("fan_control", "fan_control", "sensor") or "",
+        fan = uci:get("fan_control", "fan_control", "fan") or ""
+    }
+
+    luci.http.prepare_content("application/json")
+    luci.http.write(json.stringify(config))
+end
+
+function save_config()
+    local uci = require "luci.model.uci".cursor()
+    local json = require "luci.jsonc"
+
+    local enabled = luci.http.formvalue("enabled")
+    local sensor = luci.http.formvalue("sensor")
+    local fan = luci.http.formvalue("fan")
+
+    uci:set("fan_control", "fan_control", "enabled", enabled or "0")
+    uci:set("fan_control", "fan_control", "sensor", sensor or "")
+    uci:set("fan_control", "fan_control", "fan", fan or "")
+    uci:commit("fan_control")
+
+    -- Reload the service
+    os.execute("/etc/init.d/fan_control restart >/dev/null 2>&1 &")
+
+    luci.http.prepare_content("application/json")
+    luci.http.write(json.stringify({success = true}))
 end
 
 function list_map()
     local uci = require "luci.model.uci".cursor()
     local json = require "luci.jsonc"
-    
+
     local maps = {}
     uci:foreach("fan_control", "map", function(section)
         table.insert(maps, {
@@ -72,12 +119,12 @@ function list_map()
             speed = section.speed
         })
     end)
-    
+
     -- Sort by temperature
     table.sort(maps, function(a, b)
         return tonumber(a.temperature or 0) < tonumber(b.temperature or 0)
     end)
-    
+
     luci.http.prepare_content("application/json")
     luci.http.write(json.stringify({maps = maps}))
 end
@@ -87,30 +134,33 @@ function add_map()
     local json = require "luci.jsonc"
     local temp = luci.http.formvalue("temperature")
     local speed = luci.http.formvalue("speed")
-    
+
     if not temp or not speed then
         luci.http.prepare_content("application/json")
         luci.http.write(json.stringify({success = false, error = "Missing temperature or speed"}))
         return
     end
-    
+
     temp = tonumber(temp)
     speed = tonumber(speed)
-    
+
     if not temp or not speed or temp < 0 or speed < 0 or speed > 255 then
         luci.http.prepare_content("application/json")
         luci.http.write(json.stringify({success = false, error = "Invalid values"}))
         return
     end
-    
+
     -- Generate unique ID
     local id = "map_" .. temp .. "_" .. math.floor(os.time() % 10000)
-    
+
     uci:set("fan_control", id, "map")
     uci:set("fan_control", id, "temperature", tostring(temp))
     uci:set("fan_control", id, "speed", tostring(speed))
     uci:commit("fan_control")
-    
+
+    -- Reload the service
+    os.execute("/etc/init.d/fan_control reload >/dev/null 2>&1 &")
+
     luci.http.prepare_content("application/json")
     luci.http.write(json.stringify({success = true, id = id}))
 end
@@ -119,16 +169,19 @@ function delete_map()
     local uci = require "luci.model.uci".cursor()
     local json = require "luci.jsonc"
     local idx = luci.http.formvalue("idx")
-    
+
     if not idx then
         luci.http.prepare_content("application/json")
         luci.http.write(json.stringify({success = false, error = "Missing index"}))
         return
     end
-    
+
     uci:delete("fan_control", idx)
     uci:commit("fan_control")
-    
+
+    -- Reload the service
+    os.execute("/etc/init.d/fan_control reload >/dev/null 2>&1 &")
+
     luci.http.prepare_content("application/json")
     luci.http.write(json.stringify({success = true}))
 end
