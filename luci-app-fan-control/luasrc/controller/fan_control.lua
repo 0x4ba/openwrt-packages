c7679929df8f8ad203ebad8f6a476057e1f296f0
Kyle <aofeng73@outlook.com>
2025-12-06 23:41:22 +0800

    NEW:

--- luci-app-fan-control/luasrc/controller/fan_control.lua
+++ luci-app-fan-control/luasrc/controller/fan_control.lua
@@ -6,4 +6,57 @@ function index()
     end
 
     entry({"admin", "services", "fan_control"}, cbi("fan_control"), _("Fan Control"), 90).dependent = true
+    entry({"admin", "services", "fan_control", "status"}, call("get_status")).leaf = true
+end
+
+function get_status()
+    local uci = require "luci.model.uci".cursor()
+    local fs = require "nixio.fs"
+    local json = require "luci.jsonc"
+    
+    local response = {}
+    
+    -- Get current temperature
+    local sensor = uci:get("fan_control", "fan_control", "sensor")
+    if sensor and fs.access(sensor) then
+        for file in fs.dir(sensor) do
+            if file:match("temp%d+_input") then
+                local temp_file = sensor .. "/" .. file
+                if fs.access(temp_file) then
+                    local raw_temp = fs.readfile(temp_file)
+                    if raw_temp then
+                        local temp_c = tonumber(raw_temp) / 1000
+                        response.current_temp = string.format("%.1f", temp_c)
+                    end
+                end
+                break
+            end
+        end
+    end
+    
+    -- Get current fan speed
+    local fan = uci:get("fan_control", "fan_control", "fan")
+    if fan and fs.access(fan) then
+        for file in fs.dir(fan) do
+            if file:match("pwm%d+$") then
+                local pwm_file = fan .. "/" .. file
+                if fs.access(pwm_file) then
+                    local raw_pwm = fs.readfile(pwm_file)
+                    if raw_pwm then
+                        local pwm_val = tonumber(raw_pwm)
+                        -- Assuming PWM is 0-255, convert to percentage
+                        local speed_pct = math.floor((pwm_val / 255) * 100)
+                        response.current_fan_speed = speed_pct
+                    end
+                end
+                break
+            end
+        end
+    end
+    
+    response.enabled = uci:get("fan_control", "fan_control", "enabled") == "1"
+    response.poll_interval = uci:get("fan_control", "advanced", "poll_interval") or "10"
+    
+    luci.http.prepare_content("application/json")
+    luci.http.write(json.stringify(response))
 end
--- luci-app-fan-control/luasrc/model/cbi/fan_control.lua
+++ luci-app-fan-control/luasrc/model/cbi/fan_control.lua
@@ -1,17 +1,44 @@
+local sys = require "luci.sys"
+local fs = require "nixio.fs"
+
 m = Map("fan_control", translate("Fan Control"), translate("Configure system fan speed based on temperature sensors"))
 
-s = m:section(TypedSection, "fan_control", translate("Settings"))
+-- Status section
+status = m:section(NamedSection, "status", "status", translate("Status"))
+status.addremove = false
+
+current_temp = status:option(DummyValue, "current_temp", translate("Current Temperature"))
+current_temp.rawhtml = true
+function current_temp.cfgvalue()
+    local temp_file = "/tmp/fan_control_temp"
+    if fs.access(temp_file) then
+        return fs.readfile(temp_file):gsub("\n", "") .. " °C"
+    end
+    return "N/A"
+end
+
+current_fan_speed = status:option(DummyValue, "current_fan_speed", translate("Current Fan Speed"))
+current_fan_speed.rawhtml = true
+function current_fan_speed.cfgvalue()
+    local speed_file = "/tmp/fan_control_speed"
+    if fs.access(speed_file) then
+        return fs.readfile(speed_file):gsub("\n", "") .. " %"
+    end
+    return "N/A"
+end
+
+-- Settings section
+s = m:section(TypedSection, "fan_control", translate("Basic Settings"))
 s.anonymous = true
 s.addremove = false
 
-e = s:option(Flag, "enabled", translate("Enable"))
+e = s:option(Flag, "enabled", translate("Enable Fan Control"))
 e.rmempty = false
+e.description = translate("Enable or disable automatic fan control")
 
--- Scan for sensors
+-- Scan for sensors and fans
 local sensors = {}
 local fans = {}
-local sys = require "luci.sys"
-local fs = require "nixio.fs"
 
 local hwmon_dir = "/sys/class/hwmon"
 if fs.access(hwmon_dir) then
@@ -27,12 +54,6 @@ if fs.access(hwmon_dir) then
                 end
             end
             
-            -- Check for temp input
-            -- We assume if it has temp*_input it can be a sensor
-            -- And if it has pwm* it can be a fan
-            -- This is a simplification, but fits the requirement
-            
-            -- Check for any temp input
             local has_temp = false
             for subfile in fs.dir(path) do
                 if subfile:match("temp%d+_input") then
@@ -45,7 +66,6 @@ if fs.access(hwmon_dir) then
                 sensors[path] = name .. " (" .. file .. ")"
             end
             
-            -- Check for any pwm output
             local has_pwm = false
             for subfile in fs.dir(path) do
                 if subfile:match("pwm%d+") then
@@ -62,13 +82,77 @@ if fs.access(hwmon_dir) then
 end
 
 sensor = s:option(ListValue, "sensor", translate("Temperature Sensor"))
+sensor.description = translate("Select the temperature sensor to monitor")
 for path, name in pairs(sensors) do
     sensor:value(path, name)
 end
 
-fan = s:option(ListValue, "fan", translate("Fan"))
+fan = s:option(ListValue, "fan", translate("Fan Device"))
+fan.description = translate("Select the fan device to control")
 for path, name in pairs(fans) do
     fan:value(path, name)
 end
+
+-- Temperature thresholds section
+therm = m:section(TypedSection, "thermal", translate("Temperature Thresholds"))
+therm.addremove = true
+therm.anonymous = false
+therm.template = "cbi/tblsection"
+
+therm_name = therm:option(Value, "name", translate("Profile Name"))
+therm_name.rmempty = false
+
+therm_target = therm:option(Value, "target_temp", translate("Target Temperature (°C)"))
+therm_target.rmempty = false
+therm_target.datatype = "uinteger"
+therm_target.description = translate("Temperature at which to adjust fan speed")
+
+therm_min_speed = therm:option(Value, "min_speed", translate("Minimum Speed (%)"))
+therm_min_speed.rmempty = false
+therm_min_speed.datatype = "range(0, 100)"
+therm_min_speed.description = translate("Minimum fan speed percentage")
+
+therm_max_speed = therm:option(Value, "max_speed", translate("Maximum Speed (%)"))
+therm_max_speed.rmempty = false
+therm_max_speed.datatype = "range(0, 100)"
+therm_max_speed.description = translate("Maximum fan speed percentage")
+
+-- Fan speed levels section
+levels = m:section(TypedSection, "speed_level", translate("Fan Speed Levels"))
+levels.addremove = true
+levels.anonymous = false
+levels.template = "cbi/tblsection"
+
+level_name = levels:option(Value, "name", translate("Level Name"))
+level_name.rmempty = false
+
+level_speed = levels:option(Value, "speed", translate("Speed (%)"))
+level_speed.rmempty = false
+level_speed.datatype = "range(0, 100)"
+
+level_temp_min = levels:option(Value, "temp_min", translate("Min Temp (°C)"))
+level_temp_min.rmempty = false
+level_temp_min.datatype = "uinteger"
+
+level_temp_max = levels:option(Value, "temp_max", translate("Max Temp (°C)"))
+level_temp_max.rmempty = false
+level_temp_max.datatype = "uinteger"
+
+-- Advanced settings section
+adv = m:section(TypedSection, "advanced", translate("Advanced Settings"))
+adv.anonymous = true
+adv.addremove = false
+
+poll_interval = adv:option(Value, "poll_interval", translate("Poll Interval (seconds)"))
+poll_interval.rmempty = false
+poll_interval.datatype = "uinteger"
+poll_interval.default = 10
+poll_interval.description = translate("How often to check temperature")
+
+hysteresis = adv:option(Value, "hysteresis", translate("Temperature Hysteresis (°C)"))
+hysteresis.rmempty = false
+hysteresis.datatype = "uinteger"
+hysteresis.default = 2
+hysteresis.description = translate("Prevent rapid fan speed changes")
 
 return m
